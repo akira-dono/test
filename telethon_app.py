783700dcd511c52bc259520be92cc876735c3397
RIXIN101 <maksim1237934@gmail.com>
2023-11-14 19:21:18 +0500

    add my comment

--- telethon_app.py
+++ telethon_app.py
@@ -5,7 +5,7 @@ from bot import bot;
 # from telethon.tl.functions.channels import JoinChannelRequest
 
 client = TelegramClient('tg_session', api_id, api_hash, system_version='4.16.30-vxCUSTOM')
-
+#!some commenty
 async def check_chat_type(chat_id):
     entity = await client.get_entity(chat_id)
     if isinstance(entity, types.User):
